c3b3a7e5fb526e3512b7bdce5f05be95126004a4
Alan Cho <alancho397@gmail.com>
2024-05-10 13:02:17 -0500

    grabs the channelId and searches the channel for most recent video

--- dist/app.js
+++ dist/app.js
@@ -1,9 +1,63 @@
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 require("dotenv").config();
 const axios = require("axios").default;
 // Test if the API works
-axios
-    .get(`https://www.googleapis.com/youtube/v3/videos?id=Pz1Hl7NHlOU&key=${process.env.API_KEY}`)
-    .then(function (response) {
-    console.log(response);
-});
+// axios
+//   .get(
+//     `https://www.googleapis.com/youtube/v3/videos?id=Pz1Hl7NHlOU&key=${process.env.API_KEY}`
+//   )
+//   .then(function (response) {
+//     console.log(response.data);
+//   });
+function getChannelId(channelName) {
+    return __awaiter(this, void 0, void 0, function* () {
+        try {
+            const response = yield axios.get("https://www.googleapis.com/youtube/v3/search", {
+                params: {
+                    part: "snippet",
+                    q: channelName,
+                    type: "channel",
+                    key: process.env.API_KEY,
+                },
+            });
+            const channelId = response.data.items[0].id.channelId;
+            return channelId;
+        }
+        catch (e) {
+            console.log("Error Occurred: ", e);
+            return null;
+        }
+    });
+}
+function getRecentVideo(channelId) {
+    return __awaiter(this, void 0, void 0, function* () {
+        try {
+            const response = yield axios.get("https://www.googleapis.com/youtube/v3/search", {
+                params: {
+                    part: "snippet",
+                    channelId: channelId,
+                    key: process.env.API_KEY,
+                    order: "date",
+                    type: "video",
+                    maxResults: 1,
+                },
+            });
+            const videoId = response.data.items[0].id.videoId;
+            console.log(videoId);
+        }
+        catch (e) {
+            console.log(e);
+            return null;
+        }
+    });
+}
+getChannelId("esselleanderic").then((channelId) => getRecentVideo(channelId));
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,48 @@
 require("dotenv").config();
 const axios = require("axios").default;
 
-// Test if the API works
-axios
-  .get(
-    `https://www.googleapis.com/youtube/v3/videos?id=Pz1Hl7NHlOU&key=${process.env.API_KEY}`
-  )
-  .then(function (response) {
-    console.log(response);
-  });
+async function getChannelId(channelName: string): Promise<string | null> {
+  try {
+    const response = await axios.get(
+      "https://www.googleapis.com/youtube/v3/search",
+      {
+        params: {
+          part: "snippet",
+          q: channelName,
+          type: "channel",
+          key: process.env.API_KEY,
+        },
+      }
+    );
+    const channelId = response.data.items[0].id.channelId;
+    return channelId;
+  } catch (e) {
+    console.log("Error Occurred: ", e);
+    return null;
+  }
+}
+
+async function getRecentVideo(channelId: string): Promise<string | null> {
+  try {
+    const response = await axios.get(
+      "https://www.googleapis.com/youtube/v3/search",
+      {
+        params: {
+          part: "snippet",
+          channelId: channelId,
+          order: "date",
+          type: "video",
+          maxResults: 1,
+          key: process.env.API_KEY,
+        },
+      }
+    );
+    const videoId = response.data.items[0].id.videoId;
+    return videoId;
+  } catch (e) {
+    console.log("Error Occurred: ", e);
+    return null;
+  }
+}
+
+getChannelId("esselleanderic").then((channelId) => getRecentVideo(channelId));
