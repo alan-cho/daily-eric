37582c13745d42b81763986de38a4863f0b8736e
Alan Cho <alancho397@gmail.com>
2024-05-10 12:30:46 -0500

    load data from api

--- dist/app.js
+++ dist/app.js
@@ -1,3 +1,9 @@
 require("dotenv").config();
-console.log(process.env.API_KEY);
+const axios = require("axios").default;
+// Test if the API works
+axios
+    .get(`https://www.googleapis.com/youtube/v3/videos?id=Pz1Hl7NHlOU&key=${process.env.API_KEY}`)
+    .then(function (response) {
+    console.log(response);
+});
 //# sourceMappingURL=app.js.map
